37e1a2b2cc5e8f0336c2ee25f5b0e8222cc49a88
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-10-17 03:44:56 +0530

    main complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from "./main/main.component";
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
